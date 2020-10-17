02ae8f6760fecb581ccb786bb7bb0c15c6bd54f3
Rick Wang <rickwangtw@hotmail.com>
2020-10-17 16:44:33 -0700

    MainActivity: use IMPORTANT_DEFAULT for the NotificationChannel

--- app/src/main/java/com/mysticwind/linenotificationsupport/MainActivity.java
+++ app/src/main/java/com/mysticwind/linenotificationsupport/MainActivity.java
@@ -136,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_LOW;
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
             channel.setDescription(CHANNEL_DESCRIPTION);
             // Register the channel with the system; you can't change the importance
