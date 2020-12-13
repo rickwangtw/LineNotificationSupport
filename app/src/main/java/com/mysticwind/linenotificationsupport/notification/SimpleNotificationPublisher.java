2862113ef2679b6774b42a2a579dabb98b63d9a0
Rick Wang <rickwangtw@hotmail.com>
2020-12-13 10:01:13 -0800

    SimpleNotificationPublisher: use the preference of using legacy sticker loader

--- app/src/main/java/com/mysticwind/linenotificationsupport/notification/SimpleNotificationPublisher.java
+++ app/src/main/java/com/mysticwind/linenotificationsupport/notification/SimpleNotificationPublisher.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.service.notification.StatusBarNotification;
 
 import com.mysticwind.linenotificationsupport.model.LineNotification;
+import com.mysticwind.linenotificationsupport.preference.PreferenceProvider;
+import com.mysticwind.linenotificationsupport.utils.BigPictureStyleImageSupportedNotificationPublisherAsyncTask;
 import com.mysticwind.linenotificationsupport.utils.GroupIdResolver;
 import com.mysticwind.linenotificationsupport.utils.MessageStyleImageSupportedNotificationPublisherAsyncTask;
 
@@ -12,19 +14,27 @@ public class SimpleNotificationPublisher implements NotificationPublisher {
     private final Context context;
     private final String packageName;
     private final GroupIdResolver groupIdResolver;
+    private final PreferenceProvider preferenceProvider;
 
     public SimpleNotificationPublisher(final Context context,
                                        final String packageName,
-                                       final GroupIdResolver groupIdResolver) {
+                                       final GroupIdResolver groupIdResolver,
+                                       final PreferenceProvider preferenceProvider) {
         this.context = context;
         this.packageName = packageName;
         this.groupIdResolver = groupIdResolver;
+        this.preferenceProvider = preferenceProvider;
     }
 
     @Override
     public void publishNotification(final LineNotification lineNotification, final int notificationId) {
-        new MessageStyleImageSupportedNotificationPublisherAsyncTask(context, lineNotification, notificationId)
-                .execute();
+        if (preferenceProvider.shouldUseLegacyStickerLoader()) {
+            new BigPictureStyleImageSupportedNotificationPublisherAsyncTask(context, lineNotification, notificationId)
+                    .execute();
+        } else {
+            new MessageStyleImageSupportedNotificationPublisherAsyncTask(context, lineNotification, notificationId)
+                    .execute();
+        }
     }
 
     @Override
