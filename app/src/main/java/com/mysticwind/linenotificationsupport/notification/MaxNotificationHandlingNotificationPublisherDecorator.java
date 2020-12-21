3cc1b541c2bc0e73e4dca351a63abc7bc098cb03
Rick Wang <rickwangtw@hotmail.com>
2020-12-20 20:10:26 -0800

    MaxNotificationHandlingNotificationPublisherDecorator: hack to publish notifications when there are still slots for the queue

--- app/src/main/java/com/mysticwind/linenotificationsupport/notification/MaxNotificationHandlingNotificationPublisherDecorator.java
+++ app/src/main/java/com/mysticwind/linenotificationsupport/notification/MaxNotificationHandlingNotificationPublisherDecorator.java
@@ -18,6 +18,7 @@ public class MaxNotificationHandlingNotificationPublisherDecorator implements No
 
     // without the cool down, messages may not get sent if messages of the same group was just dismissed
     private static final long DISMISS_COOL_DOWN_IN_MILLIS = 500L;
+    private static final long NOTIFICATION_CHECK_PERIOD_IN_MILLIS = 500L;
 
     // tracks the messages that has not been sent
     private static final ConcurrentLinkedQueue<QueueItem> QUEUE_ITEMS = new ConcurrentLinkedQueue<>();
@@ -83,24 +84,62 @@ public class MaxNotificationHandlingNotificationPublisherDecorator implements No
             return;
         }
         final long delayInMillis = calculateDelayInMillis(firstItem.get().getLineNotification().getChatId());
+        delayedPublish(firstItem.get(), delayInMillis);
+        CHAT_ID_TO_LAST_DISMISSED_INSTANT_MAP.put(chatId, Instant.now().plusMillis(delayInMillis));
+
+        // TODO why are there cases where there are available slots but the queue still has items???
+        if (!QUEUE_ITEMS.isEmpty()) {
+            scheduleSlotCheck(delayInMillis + NOTIFICATION_CHECK_PERIOD_IN_MILLIS);
+        }
+    }
+
+    private void delayedPublish(final QueueItem queueItem, final long delayInMillis) {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Timber.d(String.format("Publishing notification (after delay of %d): %s",
-                        delayInMillis, firstItem.get().getLineNotification().getMessage()));
-                publish(firstItem.get().getLineNotification(), firstItem.get().getNotificationId());
+                        delayInMillis, queueItem.getLineNotification().getMessage()));
+                publish(queueItem.getLineNotification(), queueItem.getNotificationId());
             }
         }, delayInMillis);
-        CHAT_ID_TO_LAST_DISMISSED_INSTANT_MAP.put(chatId, Instant.now().plusMillis(delayInMillis));
     }
 
     private long calculateDelayInMillis(final String chatId) {
-        final long lastDismissedTimestamp = CHAT_ID_TO_LAST_DISMISSED_INSTANT_MAP.getOrDefault(chatId, Instant.now()).toEpochMilli();
-        final long now = Instant.now().toEpochMilli();
+        final long lastDismissedTimestamp = CHAT_ID_TO_LAST_DISMISSED_INSTANT_MAP.getOrDefault(chatId, Instant.now()).toEpochMilli(); final long now = Instant.now().toEpochMilli();
         if (lastDismissedTimestamp > now) {
             return lastDismissedTimestamp - now + 1;
         } else {
             return DISMISS_COOL_DOWN_IN_MILLIS;
+        }
+    }
+
+    private void scheduleSlotCheck(final long delayInMillis) {
+        Timber.d("Scheduling slot checks ...");
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                republishIfSlotsAvailable();
+            }
+        }, delayInMillis);
+    }
+
+    private void republishIfSlotsAvailable() {
+        Timber.d("Running slot checks ...");
+        final QueueItem item = QUEUE_ITEMS.peek();
+        if (item == null) {
+            return;
+        }
+        if (notificationCounter.hasSlot(item.getLineNotification().getChatId())) {
+            QUEUE_ITEMS.remove(item);
+
+            final long delayInMillis = calculateDelayInMillis(item.getLineNotification().getChatId());
+            Timber.d(String.format("Scheduled notification publishing (after delay of %d): %s",
+                    delayInMillis, item.getLineNotification().getMessage()));
+            delayedPublish(item, delayInMillis);
+
+            if (!QUEUE_ITEMS.isEmpty()) {
+                scheduleSlotCheck(delayInMillis + NOTIFICATION_CHECK_PERIOD_IN_MILLIS);
+            }
         }
     }
 
