b8f03004a3ef78c09b684c3a344cbdeaf8198892
Rick Wang <rickwangtw@hotmail.com>
2020-12-20 22:07:16 -0800

    SettingsActivity: show the dialog to take users to LINE notifiation channel settings if the app is going to manage LINE notifications

--- app/src/main/java/com/mysticwind/linenotificationsupport/SettingsActivity.java
+++ app/src/main/java/com/mysticwind/linenotificationsupport/SettingsActivity.java
@@ -1,17 +1,23 @@
 package com.mysticwind.linenotificationsupport;
 
+import android.app.Dialog;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
 import com.mysticwind.linenotificationsupport.android.AndroidFeatureProvider;
+import com.mysticwind.linenotificationsupport.line.Constants;
 import com.mysticwind.linenotificationsupport.notificationgroup.NotificationGroupCreator;
 import com.mysticwind.linenotificationsupport.preference.PreferenceProvider;
 
@@ -23,26 +29,32 @@ public class SettingsActivity extends AppCompatActivity {
 
         private static final String MERGE_NOTIFICATION_CHANNEL_PREFERENCE_KEY = "merge_message_notification_channels";
 
+        private Dialog silentLineMessageNotificationSettingsDialog;
+        private Dialog alertLineMessageNotificationSettingsDialog;
+
         private final SharedPreferences.OnSharedPreferenceChangeListener onPreferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
 
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String preferenceKey) {
-                if (!StringUtils.equals(MERGE_NOTIFICATION_CHANNEL_PREFERENCE_KEY, preferenceKey)) {
-                    return;
+                if (StringUtils.equals(MERGE_NOTIFICATION_CHANNEL_PREFERENCE_KEY, preferenceKey)) {
+                    final NotificationGroupCreator notificationGroupCreator = new NotificationGroupCreator(
+                            (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE),
+                            new AndroidFeatureProvider(), getPreferenceProvider());
+
+                    boolean shouldMergeNotification = sharedPreferences.getBoolean(preferenceKey, false);
+                    if (shouldMergeNotification) {
+                        notificationGroupCreator.migrateToSingleNotificationChannelForMessages();
+                    } else {
+                        notificationGroupCreator.migrateToMultipleNotificationChannelsForMessages();
+                    }
                 }
-
-                final NotificationGroupCreator notificationGroupCreator = new NotificationGroupCreator(
-                        (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE),
-                        new AndroidFeatureProvider(), getPreferenceProvider());
-
-                boolean shouldMergeNotification = sharedPreferences.getBoolean(preferenceKey, false);
-                if (shouldMergeNotification) {
-                    notificationGroupCreator.migrateToSingleNotificationChannelForMessages();
-                } else {
-                    notificationGroupCreator.migrateToMultipleNotificationChannelsForMessages();
+                if (PreferenceProvider.MANAGE_LINE_MESSAGE_NOTIFICATIONS_PREFERENCE_KEY.equals(preferenceKey)) {
+                    if (getPreferenceProvider().shouldManageLineMessageNotifications()) {
+                        silentLineMessageNotificationSettingsDialog.show();
+                    } else {
+                        alertLineMessageNotificationSettingsDialog.show();
+                    }
                 }
-
-                // TODO if we have chosen to manage LINE notification, we should also mute the notifications
             }
 
             private PreferenceProvider getPreferenceProvider() {
@@ -60,6 +72,13 @@ public class SettingsActivity extends AppCompatActivity {
         public void onResume() {
             super.onResume();
 
+            if (silentLineMessageNotificationSettingsDialog == null) {
+                silentLineMessageNotificationSettingsDialog = createSilentLineMessageNotificationSettingsDialog();
+            }
+            if (alertLineMessageNotificationSettingsDialog == null) {
+                alertLineMessageNotificationSettingsDialog = createAlertLineMessageNotificationSettingsDialog();
+            }
+
             getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(onPreferenceChangeListener);
         }
 
@@ -68,6 +87,38 @@ public class SettingsActivity extends AppCompatActivity {
             super.onPause();
 
             getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(onPreferenceChangeListener);
+        }
+
+        private Dialog createSilentLineMessageNotificationSettingsDialog() {
+            return new AlertDialog.Builder(getContext())
+                    .setMessage(R.string.make_line_message_notification_silent)
+                    .setPositiveButton(R.string.go_to_settings, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS)
+                                    .putExtra(Settings.EXTRA_APP_PACKAGE, Constants.LINE_PACKAGE_NAME)
+                                    .putExtra(Settings.EXTRA_CHANNEL_ID, Constants.NEW_MESSAGE_NOTIFICATION_CHANNEL_NAME);
+                            startActivity(intent);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setCancelable(true)
+                    .create();
+        }
+
+        private Dialog createAlertLineMessageNotificationSettingsDialog() {
+            return new AlertDialog.Builder(getContext())
+                    .setMessage(R.string.reminder_for_enabling_line_message_notification)
+                    .setPositiveButton(R.string.go_to_settings, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS)
+                                    .putExtra(Settings.EXTRA_APP_PACKAGE, Constants.LINE_PACKAGE_NAME)
+                                    .putExtra(Settings.EXTRA_CHANNEL_ID, Constants.NEW_MESSAGE_NOTIFICATION_CHANNEL_NAME);
+                            startActivity(intent);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setCancelable(true)
+                    .create();
         }
 
     }
@@ -86,7 +137,6 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
     }
 
     @Override
--- app/src/main/java/com/mysticwind/linenotificationsupport/line/Constants.java
+++ app/src/main/java/com/mysticwind/linenotificationsupport/line/Constants.java
@@ -3,5 +3,6 @@ package com.mysticwind.linenotificationsupport.line;
 public class Constants {
 
     public static final String LINE_PACKAGE_NAME = "jp.naver.line.android";
+    public static final String NEW_MESSAGE_NOTIFICATION_CHANNEL_NAME = "jp.naver.line.android.notification.NewMessages";
 
 }
