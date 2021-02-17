272e231e947825ebc3f75835b964abfb49ea3738
Rick Wang <rickwangtw@hotmail.com>
2021-02-16 20:43:49 -0800

    LinkActionInjectorNotificationPublisherDecorator: inject website link title in the message

--- app/src/main/java/com/mysticwind/linenotificationsupport/notification/LinkActionInjectorNotificationPublisherDecorator.java
+++ app/src/main/java/com/mysticwind/linenotificationsupport/notification/LinkActionInjectorNotificationPublisherDecorator.java
@@ -5,15 +5,21 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.service.notification.StatusBarNotification;
 
 import com.mysticwind.linenotificationsupport.R;
 import com.mysticwind.linenotificationsupport.model.LineNotification;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Objects;
 import java.util.Optional;
+
+import timber.log.Timber;
 
 public class LinkActionInjectorNotificationPublisherDecorator implements NotificationPublisher {
 
@@ -33,10 +39,29 @@ public class LinkActionInjectorNotificationPublisherDecorator implements Notific
             return;
         }
 
-        final LineNotification linkActionInjectedLineNotification = lineNotification.toBuilder()
-                .action(buildLinkAction(url.get()))
-                .build();
-        this.notificationPublisher.publishNotification(linkActionInjectedLineNotification, notificationId);
+        asyncFetchTitleAndPublish(lineNotification, notificationId, url.get());
+    }
+
+    private void asyncFetchTitleAndPublish(final LineNotification lineNotification,
+                                           final int notificationId,
+                                           final String url) {
+        new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                Optional<String> title = getTitle(url);
+                return title.orElse(null);
+            }
+
+            @Override
+            protected void onPostExecute(String title) {
+                final LineNotification linkActionInjectedLineNotification = lineNotification.toBuilder()
+                        .action(buildLinkAction(url))
+                        .message(injectUrlTitle(lineNotification.getMessage(), url, title))
+                        .build();
+                notificationPublisher.publishNotification(linkActionInjectedLineNotification, notificationId);
+            }
+        }.execute();
     }
 
     private Optional<String> findUrl(final String message) {
@@ -86,6 +111,28 @@ public class LinkActionInjectorNotificationPublisherDecorator implements Notific
         final String buttonText = context.getString(R.string.link_button_text);
         return new Notification.Action.Builder(android.R.drawable.btn_default, buttonText, pendingIntent)
                 .build();
+    }
+
+    // TODO move this to a separate decorator/reactor
+    // TODO support injecting when there are multiple URLs
+    private String injectUrlTitle(final String message, final String url, final String title) {
+        if (title == null) {
+            return message;
+        }
+        final int endIndex = message.indexOf(url) + url.length();
+        final String firstPart = message.substring(0, endIndex);
+        final String lastPart = message.substring(firstPart.length());
+        return String.format("%s (%s) %s", firstPart, title, lastPart);
+    }
+
+    private Optional<String> getTitle(String url) {
+        try {
+            final Document document = Jsoup.connect(url).get();
+            return Optional.ofNullable(document.title());
+        } catch (Exception e) {
+            Timber.e(e, "Failed to extract title from url [%s]: [%s]", url, e.getMessage());
+            return Optional.empty();
+        }
     }
 
     @Override
