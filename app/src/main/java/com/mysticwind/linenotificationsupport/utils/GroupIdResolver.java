25a07f416ca49b9ef04264579292fe6162b65bcb
Rick Wang <rickwangtw@hotmail.com>
2021-05-11 20:38:11 -0700

    GroupIdResolver: use the hash codes of chat IDs to calculate the group IDs

--- app/src/main/java/com/mysticwind/linenotificationsupport/utils/GroupIdResolver.java
+++ app/src/main/java/com/mysticwind/linenotificationsupport/utils/GroupIdResolver.java
@@ -1,13 +1,16 @@
 package com.mysticwind.linenotificationsupport.utils;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import timber.log.Timber;
 
 public class GroupIdResolver {
 
     private static final int GROUP_ID_START = 0x4000;
 
-    private final Map<String, Integer> chatIdToGroupIdMap = new HashMap<>();
+    private final Map<Integer, String> groupIdToChatIdMap = new ConcurrentHashMap<>();
+    private final Map<String, Integer> fallbackChatIdToGroupIdMap = new ConcurrentHashMap<>();
     private int lastGroupId = GROUP_ID_START;
 
     public GroupIdResolver() {
@@ -18,9 +21,21 @@ public class GroupIdResolver {
     }
 
     public int resolveGroupId(final String chatId) {
-        return chatIdToGroupIdMap.computeIfAbsent(chatId, chatIdWithoutGroupId ->
-            getNextLastGroupId()
-        );
+        final int calculatedGroupId = chatId.hashCode();
+        final String storedChatId = groupIdToChatIdMap.get(calculatedGroupId);
+        if (storedChatId == null) {
+            groupIdToChatIdMap.put(calculatedGroupId, chatId);
+            return calculatedGroupId;
+        } else if (storedChatId.equals(chatId)) {
+            return calculatedGroupId;
+        } else {
+            // fallback that should almost never happen
+            final int selectedGroupId = fallbackChatIdToGroupIdMap.computeIfAbsent(chatId, id -> getNextLastGroupId());
+            // TODO what if there is a clash with the group IDs and the hash codes?
+            Timber.w("Chat ID [%s] hash [%d] has been used, using group ID [%d] instead",
+                    chatId, calculatedGroupId, selectedGroupId);
+            return selectedGroupId;
+        }
     }
 
     private synchronized int getNextLastGroupId() {
