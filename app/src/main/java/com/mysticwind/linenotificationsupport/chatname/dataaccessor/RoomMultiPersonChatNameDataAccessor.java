389586c0c44a254b796a35c6102f43d056ea41de
Rick Wang <rickwangtw@hotmail.com>
2021-05-12 21:44:28 -0700

    Implement MultiPersonChatNameDataAccessor using Room

---
+++ app/src/main/java/com/mysticwind/linenotificationsupport/chatname/dataaccessor/RoomMultiPersonChatNameDataAccessor.java
@@ -0,0 +1,46 @@
+package com.mysticwind.linenotificationsupport.chatname.dataaccessor;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.mysticwind.linenotificationsupport.persistence.ChatGroupDatabase;
+import com.mysticwind.linenotificationsupport.persistence.chatname.dto.ChatSenderEntry;
+
+import org.apache.commons.lang3.Validate;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class RoomMultiPersonChatNameDataAccessor implements MultiPersonChatNameDataAccessor {
+
+    private final ChatGroupDatabase chatGroupDatabase;
+
+    public RoomMultiPersonChatNameDataAccessor(final ChatGroupDatabase chatGroupDatabase) {
+        this.chatGroupDatabase = Objects.requireNonNull(chatGroupDatabase);
+    }
+
+    @Override
+    public String addRelationshipAndGetChatGroupName(final String chatId, final String sender) {
+        Validate.notBlank(chatId);
+        Validate.notBlank(sender);
+
+        final ChatSenderEntry entry = new ChatSenderEntry();
+        entry.setChatId(chatId);
+        entry.setSender(sender);
+        entry.setCreatedAtTimestamp(Instant.now().toEpochMilli());
+        entry.setUpdatedAtTimestamp(Instant.now().toEpochMilli());
+        chatGroupDatabase.chatSenderDao().insert(entry);
+
+        return sender;
+    }
+
+    @Override
+    public Multimap<String, String> getAllChatIdToSenders() {
+        final Multimap<String, String> chatIdToSenderMultimap = HashMultimap.create();
+
+        chatGroupDatabase.chatSenderDao().getAllEntries().forEach(
+                entry ->
+                        chatIdToSenderMultimap.put(entry.getChatId(), entry.getSender()));
+        return chatIdToSenderMultimap;
+    }
+
+}
--- app/src/main/java/com/mysticwind/linenotificationsupport/persistence/ChatGroupDatabase.java
+++ app/src/main/java/com/mysticwind/linenotificationsupport/persistence/ChatGroupDatabase.java
@@ -3,12 +3,15 @@ package com.mysticwind.linenotificationsupport.persistence;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.mysticwind.linenotificationsupport.persistence.chatname.dao.ChatSenderDao;
 import com.mysticwind.linenotificationsupport.persistence.chatname.dao.GroupChatNameDao;
+import com.mysticwind.linenotificationsupport.persistence.chatname.dto.ChatSenderEntry;
 import com.mysticwind.linenotificationsupport.persistence.chatname.dto.GroupChatNameEntry;
 
-@Database(entities = {GroupChatNameEntry.class}, version = 1)
+@Database(entities = {GroupChatNameEntry.class, ChatSenderEntry.class}, version = 1)
 public abstract class ChatGroupDatabase extends RoomDatabase {
 
     public abstract GroupChatNameDao groupChatNameDao();
+    public abstract ChatSenderDao chatSenderDao();
 
 }
